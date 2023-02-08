79c2a81da87ff6eaf5855890b91b97f6f81652d1
leonardopadilha <leonardopadilha28@gmail.com>
2023-02-08 06:00:00 -0300

    Iniciando projeto

---
+++ lista-alunos.php
@@ -0,0 +1,15 @@
+<?php
+
+use Alura\Pdo\Domain\Model\Student;
+
+require_once 'vendor/autoload.php';
+
+$databasePath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO('sqlite:' . $databasePath);
+
+$statement = $pdo->query('SELECT * FROM students');
+$studentList = $statement->fetchAll();
+
+//var_dump($statement);
+
+echo $studentList[0]['name'];
