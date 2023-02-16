64a67da5a46e4e8f79ed977496c814fbf778dbac
leonardopadilha <leonardopadilha28@gmail.com>
2023-02-16 06:19:01 -0300

    Tratamento de erros - Outros parâmetros

--- src/Infrastructure/Persistence/ConnectionCreator.php
+++ src/Infrastructure/Persistence/ConnectionCreator.php
@@ -9,6 +9,10 @@ class ConnectionCreator
     public static function createConnection() : PDO
     {
         $databasePath = __DIR__ . '/../../../banco.sqlite';
-        return new PDO('sqlite:' . $databasePath);
+        $conection = new PDO('sqlite:' . $databasePath);
+        
+        $conection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $conection;
     }
 }
